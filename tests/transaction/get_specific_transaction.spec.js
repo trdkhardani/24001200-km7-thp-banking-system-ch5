96154347ace80b3bb1ddd06f0fa160d6130138d2
Tridiktya Putra <tridiktya@gmail.com>
2024-10-27 18:40:06 +0700

    test: add tests for GET /api/v1/transactions & /api/v1/transactions/{transactionId}

---
+++ tests/transaction/get_specific_transaction.spec.js
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { describe, expect, jest } from '@jest/globals';
+import { PrismaClient } from '@prisma/client';
+import app from '../../index.js';
+
+const mockPrisma = {
+  transaction: {
+    findUnique: jest.fn(),
+  },
+};
+
+jest.mock('@prisma/client', () => ({
+  PrismaClient: jest.fn(() => mockPrisma),
+}));
+
+describe('GET /api/v1/transactions/{transactionId}', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // clear mocks between tests
+  });
+
+  it('should show specific transaction', async () => {
+
+    const res = await request(app).get('/api/v1/transactions/1');
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('success');
+  });
+
+  it('should show 404 not found', async () => {
+
+    const res = await request(app).get('/api/v1/transactions/1000');
+    
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe('failed');
+  });
+});
